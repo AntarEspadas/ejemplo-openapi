3dbe422c9f459d4f6d6c6ad43baed27f1cb87733
Antar Espadas <ae3144@nyu.edu>
2025-07-24 14:48:45 -0600

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI4NzkyOQ/spectral.js?branch_slug=master&token=316f9037-e45e-4a07-ad14-935bfb41b26a";
+export default { extends: ruleset };
